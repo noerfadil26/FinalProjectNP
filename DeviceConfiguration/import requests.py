a6fded1b831bdcc21d87171c007e7d4577d34f34
Ryan Nurfadilah <noerfadil26@gmail.com>
2022-06-30 21:40:41 +0700

    Main Files

--- DeviceConfiguration/import requests.py
+++
@@ -1,4 +0,0 @@
-import requests
-URL = "https://simak.unsil.ac.id/us-unsil/baa/transkripmhsw.php?gos=_CetakTranskrip&MhswID=197002059&_rnd=zrH5qdg1&jen=2"
-response = requests.get(URL)
-open("instagram.ico", "wb").write(response.content)
