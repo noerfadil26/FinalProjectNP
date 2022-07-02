ac9f730dd6dd850b1cfdde863eb73d9bfd317efb
Ryan Nurfadilah <noerfadil26@gmail.com>
2022-07-02 16:17:32 +0700

    Mengubah print output

--- PTAutomation/Add Credential.py
+++ PTAutomation/Add Credential.py
@@ -1,17 +1,16 @@
 import requests
 import json
+
 api_url = "http://localhost:58000/api/v1/global-credential/cli"
 headers = {
-  'X-Auth-Token': 'NC-8-080562329a864ccf8225-nbi',
+  'X-Auth-Token': 'NC-7-5924317466424992b3f2-nbi',
   'Content-Type': 'application/json'
 }
 body_json = {
   "password": "project",
   "username": "final",
   "enablePassword": "project",
-  "description": "CRED2"
+  "description": "TESTING2"
 }
-resp = requests.post(api_url,json.dumps(body_json),headers=headers)
-response_json = resp.json()
-credential=response_json
-print (" id\t:\t",credential["id"],"\n","username:\t",credential["username"],"\n","password:\t",credential["password"],"\n","enable password:\t",credential["enablePassword"])
+response = requests.post(api_url, json.dumps(body_json), headers=headers, verify=False)
+print(response.text)
